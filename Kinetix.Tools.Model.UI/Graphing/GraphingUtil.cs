2c8780712c778ac974b23bf62fc47c5ed2b08dcb
JabX <damien.frikha@outlook.com>
2020-01-24 01:07:08 +0100

    Rationalisation génération graphes

---
+++ Kinetix.Tools.Model.UI/Graphing/GraphingUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Kinetix.Tools.Model.FileModel;
+
+namespace Kinetix.Tools.Model.UI.Graphing
+{
+    public static class GraphingUtil
+    {
+        public static string GetURL(this ModelFile? file)
+        {
+            return file == null
+                ? string.Empty
+                : $"{file.Descriptor.Module}/{file.Descriptor.Kind}/{WebUtility.UrlEncode(file.Descriptor.File)}";
+        }
+
+        public static string ForTooltip(this string source)
+        {
+            return source.Replace("\"", "\\\"") ?? string.Empty;
+        }
+    }
+}
